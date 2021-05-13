9a5c584020adfa82417a1f7c35316ca4f16727d5
SungJLee <songoku3212@gmail.com>
2021-05-13 23:29:35 +0900

    This is Car

--- GitTest/Test/src/Main.java
+++ GitTest/Test/src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Car car = new Car();
+		
+		car.name = "hello";
+		car.price = 3000;
+		
+		System.out.println(car.name + " " + car.price);
 	}
 
 }
